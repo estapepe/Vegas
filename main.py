62a12871fecbdf3aef2562502643a68a436675ba
José Luis Mazariegos Ballesteros <estapepe@gmail.com>
2016-04-14 23:58:17 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+
+class Die:
+    def __init__(self, cardinality, color):
+        self.color = color
+        self.upper_surface = random.randint(1, cardinality)
+
+current_game =
+current_game.play()
+
+current_set =
+current_set.play()
+
+replicate_cardinality = 10
+alpha_experiment = VegasExperiment(replicate_cardinality, VegasExperiment.ALPHA)
+experiments = [alpha_experiment]
+current_experiment_set = VegasExperimentSet()
+current_experiment_set.execute()
